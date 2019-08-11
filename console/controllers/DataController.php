75d5ef8c863e5058f9a548dc4aae738098399887
Andrey <forweban@gmail.com>
2019-08-11 17:36:49 +0300

    added migration change_parent_id_category_table & DataController with actionAddTeatData inti category_table

---
+++ console/controllers/DataController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace console\controllers;
+
+use Ramsey\Uuid\Uuid;
+use yii\console\Controller;
+use Yii;
+use common\models\Category;
+
+
+/**
+ * Class DataController
+ * @package console\controllers
+ */
+class DataController extends Controller
+{
+    /**
+     * @throws \yii\db\Exception
+     */
+    public function actionAddTestData()
+    {
+        $uuid_root = Uuid::uuid4()->toString();
+
+        $connection = Yii::$app->db;
+
+        $connection->createCommand()->insert('category', [
+            'id' => $uuid_root,
+            'depth' => 0,
+            'name' => 'root',
+            'parent_id' => NULL,
+        ])->execute();
+
+
+        $categories = [
+            'electronics' => ['notebooks', 'laptops', 'monitors', 'mobile'],
+            'photos' => ['DSLR', 'lens', 'videos', 'bags'],
+            'sport' => ['fishing', 'hiking', 'closes'],
+            'kinders' => ['plays', 'books', 'boots'],
+        ];
+
+        foreach($categories as $category => $subCategories) {
+
+            $uuid_cat = Uuid::uuid4()->toString();
+
+            $connection->createCommand()->insert('category', [
+                'id' => $uuid_cat,
+                'depth' => 1,
+                'name' => $category,
+                'parent_id' => $uuid_root,
+            ])->execute();
+
+            foreach($subCategories as $subCategory) {
+                $connection->createCommand()->insert('category', [
+                    'id' => Uuid::uuid4()->toString(),
+                    'depth' => 2,
+                    'name' => $subCategory,
+                    'parent_id' => $uuid_cat,
+                ])->execute();
+
+            }
+        }
+    }
+
+    public function actionDelTestData() {
+
+        $category_table = new Category();
+        $category_table::deleteAll();
+
+    }
+}
---
+++ console/migrations/m190811_141526_change_parent_id_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190811_141526_change_parent_id_category_table
+ */
+class m190811_141526_change_parent_id_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->dropColumn('{{%category}}', 'parent_id');
+        $this->addColumn('{{%category}}', 'parent_id',
+                        $this->getDb()->getSchema()->createColumnSchemaBuilder('uuid')
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%category}}', 'parent_id');
+        $this->addColumn('{{%category}}', 'parent_id', $this->integer()->notNull());
+    }
+}
