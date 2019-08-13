7be51571b8f2dd4775a2166cebf184b303f89ebe
Rostyslav <urostyslawa@gmail.com>
2019-08-13 22:49:10 +0300

    Changed views & assets

--- frontend/views/site/product.php
+++ frontend/views/site/product.php
@@ -68,3 +68,58 @@ $this->params['breadcrumbs'][] = $this->title;
     </form>
 
 </section>
+<section>
+
+    <div class="product-tabs">
+        <ul class="product-tabs-options">
+            <li data-target="comments">Comments</li>
+            <li data-target="parameters">Additional parameters</li>
+            <li data-target="description" class="active">Description</li>
+        </ul>
+        <div class="product-tabs-content">
+            <div class="product-tabs-item" data-target="comments">
+                <ul class="comments-list">
+                    <li>
+                        <div class="user-photo">
+                            <img src="../img/featured.png" alt="user-photo">
+                        </div>
+
+                        <p>Very good mf item. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam cum
+                            consequatur sint, quis voluptatem vitae reiciendis eius repudiandae ullam, incidunt nemo
+                            praesentium laudantium soluta nihil maxime beatae, nostrum numquam? Unde.</p>
+                    </li>
+                    <li>
+                        <div class="user-photo">
+                            <!-- no-photo --> <img src="../img/account.png" alt="user-photo">
+                        </div>
+
+                        <p>Very good mf item. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam cum
+                            praesentium laudantium soluta nihil maxime beatae, nostrum numquam? Unde.</p>
+                    </li>
+                </ul>
+                <form action="#" method="POST" class="comments-add-form">
+                    <textarea class="comments-input"></textarea>
+                    <button class="comments-add-new">Add new comment</button>
+                </form>
+            </div>
+            <div class="product-tabs-item" data-target="parameters">
+                <ul class="parameters-list">
+                    <li>
+                        <span class="parameter-name">ParameterName</span>:<span>Parame</span>
+                    </li>
+                    <li>
+                        <span class="parameter-name">ParameterName</span>:<span>1</span>
+                    </li>
+                    <li>
+                        <span class="parameter-name">ParameterName</span>:<span>ParameterValue</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="product-tabs-item active" data-target="description">
+                <p class="product-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio
+                    cupiditate rem quam saepe reprehenderit sed voluptatibus eaque excepturi perspiciatis adipisci,
+                    officiis similique, dolor enim cum necessitatibus quos dolores impedit voluptates.</p>
+            </div>
+        </div>
+    </div>
+</section>
