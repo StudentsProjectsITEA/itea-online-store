2266439ec4d15747b6ecfcf38a42e4f3fd21c4f4
Andrey <forweban@gmail.com>
2019-08-15 02:22:35 +0300

    Added CRUD for backend/user

---
+++ backend/models/UserSearch.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace backend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\User;
+
+/**
+ * UserSearch represents the model behind the search form of `frontend\models\User`.
+ */
+class UserSearch extends User
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'username', 'first_name', 'last_name', 'auth_key', 'password_hash', 'password_reset_token', 'email', 'verification_token'], 'safe'],
+            [['mobile', 'status_id', 'created_time', 'updated_time'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = User::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'mobile' => $this->mobile,
+            'status_id' => $this->status_id,
+            'created_time' => $this->created_time,
+            'updated_time' => $this->updated_time,
+        ]);
+
+        $query->andFilterWhere(['ilike', 'id', $this->id])
+            ->andFilterWhere(['ilike', 'username', $this->username])
+            ->andFilterWhere(['ilike', 'first_name', $this->first_name])
+            ->andFilterWhere(['ilike', 'last_name', $this->last_name])
+            ->andFilterWhere(['ilike', 'auth_key', $this->auth_key])
+            ->andFilterWhere(['ilike', 'password_hash', $this->password_hash])
+            ->andFilterWhere(['ilike', 'password_reset_token', $this->password_reset_token])
+            ->andFilterWhere(['ilike', 'email', $this->email])
+            ->andFilterWhere(['ilike', 'verification_token', $this->verification_token]);
+
+        return $dataProvider;
+    }
+}
