78f5e3934e3cd074994c5a84c35225df044e81d5
Andrey <forweban@gmail.com>
2019-08-15 08:35:51 +0300

    Added CRUD for frontend/product

---
+++ frontend/views/product/form.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Product */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'quantity')->textInput() ?>
+
+    <?= $form->field($model, 'price')->textInput() ?>
+
+    <?= $form->field($model, 'main_photo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'is_deleted')->checkbox() ?>
+
+    <?= $form->field($model, 'created_time')->textInput() ?>
+
+    <?= $form->field($model, 'updated_time')->textInput() ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <?= $form->field($model, 'brand_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
