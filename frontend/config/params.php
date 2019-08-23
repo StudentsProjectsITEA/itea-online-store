f0df0ad31f10fa26a190cc3774767421a93b6fa9
Andrey <forweban@gmail.com>
2019-08-23 15:20:23 +0300

    Added count of products into params for Site & Product Controllers

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,8 +1,12 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+
     'countOfPopularProducts' => 4,
     'countOfPopularCategories' => 4,
+    'countOfProductsOnMainPage' => 3,
+    'countOfProductsOnProductsPage' => 5,
+
     'productsOrderBy' => 'title',
     'maxButtonPaginationCount' => 5,
 ];
